958112c248fb5150ca818a2b1eaf24f78c3ff27e
Firstweera <firstweera357@gmail.com>
2023-03-23 12:31:55 +0700

    feat: connect API

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,51 +2,57 @@ import React, { useState, useEffect } from "react";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import { RxDotFilled } from "react-icons/rx";
 
-export const Carousel = ({ slides, auto, interval }) => {
+export const Carousel = ({ promotion, slides, auto, interval }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  // console.log("test 2", promotion?.length);
 
   const prevSlide = () => {
-    setCurrentIndex((prev) => (prev === 0 ? slides.length - 1 : prev - 1));
+    setCurrentIndex((prev) => (prev === 0 ? promotion?.length - 1 : prev - 1));
   };
 
   const nextSlide = () => {
-    setCurrentIndex((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
+    setCurrentIndex((prev) => (prev === promotion?.length - 1 ? 0 : prev + 1));
   };
 
-  const goToSlide = (index) => {
-    setCurrentIndex(index);
+  const goToSlide = (idx) => {
+    setCurrentIndex(idx);
   };
 
   useEffect(() => {
     let intervalId;
     if (auto === true) {
       intervalId = setInterval(() => {
-        setCurrentIndex((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
+        setCurrentIndex((prev) =>
+          prev === promotion?.length - 1 ? 0 : prev + 1
+        );
       }, interval);
     }
     return () => clearInterval(intervalId);
-  }, [auto, interval, slides.length]);
+  }, [auto, interval, promotion?.length]);
 
   return (
-    <div className="max-w-[1200px] h-[500px] w-full m-auto py-16 px-4 relative group">
-      <div
-        style={{
-          backgroundImage: `url(${slides[currentIndex].url})`,
-          transition: "background-image 0.5s ease-in-out",
-        }}
-        className="w-full h-full rounded-2xl bg-center bg-cover transition-transform duration-500 ease-in-out"
-      ></div>
+    <div className="max-w-[1200px] h-[350px] md:h-[500px] w-full m-auto py-16 px-4 relative group">
+      {currentIndex > 0 && (
+        <div
+          style={{
+            backgroundImage: `url(${promotion[currentIndex]?.image})`,
+            transition: "background-image 0.5s ease-in-out",
+          }}
+          className="w-full h-full rounded-2xl bg-center bg-cover transition-transform duration-500 ease-in-out"
+        ></div>
+      )}
+
       {/* Left Arrow */}
-      <div className="hidden group-hover:block absolute top-[45%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+      <div className="hidden group-hover:block absolute top-[40%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
         <BsChevronCompactLeft onClick={prevSlide} size={30} />
       </div>
       {/* Right Arrow */}
-      <div className="hidden group-hover:block absolute top-[45%] -translate-x-0 translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+      <div className="hidden group-hover:block absolute top-[40%] -translate-x-0 translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
         <BsChevronCompactRight onClick={nextSlide} size={30} />
       </div>
-      <div className="absolute top-[80%] right-0 left-0">
+      <div className="absolute top-[70%] md:top-[80%] right-0 left-0">
         <div className="flex top-4 justify-center py-2">
-          {slides.map((r, idx) => (
+          {promotion?.map((r, idx) => (
             <div
               key={idx}
               onClick={() => goToSlide(idx)}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Carousel } from "../components/Carousel";
+import axios from "axios";
 
 export const Home = () => {
   const slides = [
@@ -20,9 +21,28 @@ export const Home = () => {
     },
   ];
 
+  const [promotion, setPromotion] = useState();
+  console.log("test", promotion?.length);
+
+  const getPromotion = async () => {
+    const result = await axios.post(
+      "https://shabudule-api.vercel.app/function/getPromotionShabudule"
+    );
+    console.log("Promotion Api", result.data);
+    setPromotion(result.data);
+  };
+  useEffect(() => {
+    getPromotion();
+  }, []);
+
   return (
     <div>
-      <Carousel slides={slides} auto={true} interval={4000} />
+      <Carousel
+        slides={slides}
+        promotion={promotion}
+        auto={true}
+        interval={3000}
+      />
     </div>
   );
 };
